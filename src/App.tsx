1790f1daffe8a6640a159f5b95eb7328c4fb660d
Guilherme Donato <163470077+gui-donato@users.noreply.github.com>
2025-08-05 06:14:07 -0300

    Atualizações com css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
  
-import { Heading } from './components/Heading';
+
   import './styles/theme.css'
 import './styles/global.css'
+
    export function App() {
-        console.log('oi');
+       
         return (
-        <><Heading /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ab error ratione nobis placeat aliquam sunt quas sapiente omnis vitae, cumque necessitatibus reiciendis voluptatum dicta possimus, cupiditate, totam ad reprehenderit.</p></>
+        <>
+        
+        <div className='container'>
+          <div className="content">
+            <section>LOGO</section>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="content">
+            <section>MENU</section>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="content">
+            <section>FORM</section>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="content">
+            <section>FOOTER</section>
+          </div>
+        </div>
+        </>
     
 )
 
